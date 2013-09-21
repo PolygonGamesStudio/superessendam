85bc04b382da93bb0ace0445702fffef97c689ad
sawa2012 <sawa2012@gmail.com>
2013-09-21 10:43:34 +0400

    simple web-server

---
+++ src/main/java/stat/Main.java
@@ -0,0 +1,13 @@
+package stat;
+
+import org.eclipse.jetty.server.Server;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new JettyServer());
+
+        server.start();
+        server.join();
+    }
+}
