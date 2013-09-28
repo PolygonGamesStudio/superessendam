080d20a9afe4d4ead9b73970aa2ba5c26525ca5d
maratkanov <nemesis1fear0>
2013-09-28 11:01:34 +0400

    hw2 done

--- src/main/java/hw2/p7/Frontend.java
+++ src/main/java/hw2/p7/Frontend.java
@@ -15,30 +15,46 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Frontend extends HttpServlet {
 
+    private Map<String, Long> login_id = new HashMap<>();
+    private Map<String, String> login_password = new HashMap<>();
+    public Frontend() {
+        String[] users = {"user0", "user1", "user2"};
+
+        login_password.put(users[0], "user0pwd");
+        login_password.put(users[1], "user1pwd");
+        login_password.put(users[2], "user2pwd");
+        login_id.put(users[0], Long.valueOf(0));
+        login_id.put(users[1], Long.valueOf(1));
+        login_id.put(users[2], Long.valueOf(2));
+    }
+
     private String login = "";
-    private String password = "";
-    private HttpSession localSession;   // here
+    private String password;
     private AtomicLong userIdGenerator = new AtomicLong();
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        Map<String, Object> pageVariables = new HashMap<>();
 
-        pageVariables.put("lastLogin", login);
-
-        localSession = request.getSession();
-        Long sessionId = (Long) localSession.getAttribute("sessionId");
+        HttpSession session = request.getSession();
+        Long sessionId = (Long) session.getAttribute("sessionId");
         if (sessionId == null) {
             sessionId = userIdGenerator.getAndIncrement();
-            localSession.setAttribute("sessionId", sessionId);
+            session.setAttribute("sessionId", sessionId);
         }
+
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("lastLogin", login);
         pageVariables.put("sessionId", sessionId);
 
-        // аналогично ли это sessionId ?
-        String userId = request.getSession().getId();
-        pageVariables.put("userId", userId);
+        Long id = (Long) session.getAttribute("userId");
+        if (id == null) {
+            pageVariables.put("userId", "knock-knock");
+        }
+        else {
+            pageVariables.put("userId", id);
+        }
 
         response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
     }
@@ -49,18 +65,25 @@ public class Frontend extends HttpServlet {
         password = request.getParameter("password");
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        Map<String, Object> pageVariables = new HashMap<>();
-        pageVariables.put("lastLogin", login);
 
-        Long sessionId = (Long) localSession.getAttribute("sessionId");
+        HttpSession session = request.getSession();
+        Long sessionId = (Long) session.getAttribute("sessionId");
         if (sessionId == null) {
             sessionId = userIdGenerator.getAndIncrement();
-            localSession.setAttribute("sessionId", sessionId);
+            session.setAttribute("sessionId", sessionId);
         }
-        pageVariables.put("sessionId", sessionId);
 
-        String userId = request.getSession().getId();
-        pageVariables.put("userId", userId);
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("sessionId", sessionId);
+        pageVariables.put("lastLogin", login);
+        if (password.equals(login_password.get(login))) {
+            Long id = login_id.get(login);
+            pageVariables.put("userId", id);
+            session.setAttribute("userId", id);
+        }
+        else {
+            pageVariables.put("userId", "you shall not pass");
+        }
 
         response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
     }
