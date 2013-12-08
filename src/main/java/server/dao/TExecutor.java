dbb8032dc50ee1aa1e98461eadca674b9d6f8dcb
sawa2012 <sawa2012@gmail.com>
2013-12-08 21:01:01 +0400

    row work this DB, rewrite AS

---
+++ src/main/java/server/dao/TExecutor.java
@@ -0,0 +1,23 @@
+package server.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TExecutor {
+    public <T> T execQuery(Connection connection,
+                           String query,
+                           TResultHandler<T> handler)
+            throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.execute(query);
+        ResultSet result = stmt.getResultSet();
+        T value = handler.handle(result);
+        result.close();
+        stmt.close();
+
+        return value;
+    }
+
+}
